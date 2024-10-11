9e9741a739d0471d93d0aa648a7025e6b0a9084b
tedecti <wolftheguy08@gmail.com>
2024-10-11 16:56:15 +0500

    Npgsql package install

--- Eclipse/Models/ConferenceMember.cs
+++ Eclipse/Models/ConferenceMember.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Eclipse.Models;
 
-public class ConferenceMembers
+public class ConferenceMember
 {
+    [Key]
     public Guid Id { get; set; }
+    [Required]
     public string? ConferenceId { get; set; }
+    [Required]
     public string? MemberId { get; set; }
+    [Required]
     public DateTime JoinedAt { get; set; }
+    [Required]
     public bool? IsAdmin { get; set; }
     
     public Conference Conference { get; set; }
