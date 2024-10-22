589668475f5833e48e93f60d529117f59c8aaf98
tedecti <wolftheguy08@gmail.com>
2024-10-22 15:50:00 +0500

    Some error exceptions

--- Eclipse/Controllers/AuthController.cs
+++ Eclipse/Controllers/AuthController.cs
@@ -13,18 +13,25 @@ namespace Eclipse.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _authRepository;
+        private readonly IUserRepository _userRepository;
         private readonly IConfiguration _configuration;
 
-        public AuthController(IAuthRepository authRepository, IConfiguration configuration)
+        public AuthController(IAuthRepository authRepository, IConfiguration configuration, IUserRepository userRepository)
         {
             _authRepository = authRepository;
             _configuration = configuration;
+            _userRepository = userRepository;
         }
 
         [HttpPost]
         [Route("signup")]
         public async Task<ApiResponse<User>> Register([FromBody] RegisterDto registerDto)
         {
+            var existUser = _userRepository.GetUserByEmail(registerDto.Email);
+            if (existUser != null)
+            {
+                throw new AlreadyExistingException("User already exists");
+            }
             var newUser = await _authRepository.Register(registerDto);
             return new ApiResponse<User> {Message = "Success", Data = newUser};
         }
