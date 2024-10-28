12a9c1cb9be0f8ea2714f0b026972de3cb76399a
tedecti <wolftheguy08@gmail.com>
2024-10-28 17:43:01 +0500

    UserProfileDto

---
+++ Eclipse/Models/Dto/UserProfileDto.cs
@@ -0,0 +1,10 @@
+namespace Eclipse.Models.Dto;
+
+public class UserProfileDto
+{
+    public string Name { get; set; }
+    public string Username { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string? Pfp { get; set; }
+}
