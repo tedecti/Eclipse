4cbb0c7ce1e7572be7a9a1346c90377fc49e4f0f
tedecti <wolftheguy08@gmail.com>
2024-11-06 16:41:59 +0500

    Modern IFormFile Using

--- Eclipse/Controllers/UserController.cs
+++ Eclipse/Controllers/UserController.cs
@@ -16,6 +16,7 @@ public class UserController : ControllerBase
     private readonly IFileRepository _fileRepository;
     private readonly IUserRepository _userRepository;
     private readonly IUserService _userService;
+    private readonly string _successMessage = "Success";
 
     public UserController(IUserService userService, IFileRepository fileRepository, IUserRepository userRepository)
     {
@@ -35,7 +36,7 @@ public class UserController : ControllerBase
         var userId = Guid.Parse(userIdClaim.Value);
         var user = await _userService.GetUserMapped(userId);
 
-        return new ApiResponse<UserProfileDto> { Message = "Success", Data = user };
+        return new ApiResponse<UserProfileDto> { Message = _successMessage, Data = user };
     }
 
     [HttpGet]
@@ -45,7 +46,7 @@ public class UserController : ControllerBase
         var user = await _userService.GetUserMapped(userId);
         if (user == null) throw new NotFoundException("User");
 
-        return new ApiResponse<UserProfileDto> { Message = "Success", Data = user };
+        return new ApiResponse<UserProfileDto> { Message = _successMessage, Data = user };
     }
 
     [Authorize]
@@ -59,15 +60,13 @@ public class UserController : ControllerBase
         var userId = Guid.Parse(userIdClaim.Value);
         var newUser = await _userRepository.UpdateUser(userId, userProfileDto);
         if (newUser == null) throw new NotFoundException("User");
-        return new ApiResponse<User> { Message = "Success", Data = newUser };
+        return new ApiResponse<User> { Message = _successMessage, Data = newUser };
     }
 
     [Authorize]
     [HttpPost("user/pfp/upload")]
-    public async Task<ApiResponse<object>> UploadAvatar()
+    public async Task<ApiResponse<object>> UploadAvatar(IFormFile file)
     {
-        var httpRequest = HttpContext.Request;
-        var file = httpRequest.Form.Files["image"];
         var user = User.FindFirst("UserId")!.Value;
         if (user == null) throw new UnauthorizedAccessException();
 
@@ -76,6 +75,6 @@ public class UserController : ControllerBase
 
         var fileName = await _fileRepository.SaveFile(file);
         await _userRepository.UploadAvatar(userId, fileName);
-        return new ApiResponse<object> { Message = "Success", Data = fileName };
+        return new ApiResponse<object> { Message = _successMessage, Data = fileName };
     }
 }
