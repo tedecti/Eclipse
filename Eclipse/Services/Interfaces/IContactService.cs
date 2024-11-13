9c9cdeb3569a3c0532c1e2b5800b0f189b32838a
tedecti <wolftheguy08@gmail.com>
2024-11-13 11:35:29 +0500

    IContactService Update

--- Eclipse/Services/Interfaces/IContactService.cs
+++ Eclipse/Services/Interfaces/IContactService.cs
@@ -1,6 +1,10 @@
+using Eclipse.Models.Dto;
+
 namespace Eclipse.Services.Interfaces;
 
 public interface IContactService
 {
-    
+    public Task<List<ShortContactDto>?> SortContactsByLastOnline(Guid userId, string sortOrder = "desc");
+    public Task<List<ShortContactDto>?> GetMappedListOfContacts(Guid userId);
+    public Task<ShortContactDto> MapResponseOfNewContact(Guid userId, Guid contactId);
 }
