28ca0377ae2904ccf06a8ba76b68d0c4ae25aaaa
tedecti <wolftheguy08@gmail.com>
2024-11-13 11:35:17 +0500

    Mapping Contacts

--- Eclipse/Services/ContactService.cs
+++ Eclipse/Services/ContactService.cs
@@ -1,4 +1,6 @@
+using AutoMapper;
 using Eclipse.Models;
+using Eclipse.Models.Dto;
 using Eclipse.Repositories.Interfaces;
 using Eclipse.Services.Interfaces;
 
@@ -7,16 +9,37 @@ namespace Eclipse.Services;
 public class ContactService : IContactService
 {
     private readonly IContactRepository _contactRepository;
+    private readonly IMapper _mapper;
 
-    public ContactService(IContactRepository contactRepository)
+    public ContactService(IContactRepository contactRepository, IMapper mapper)
     {
         _contactRepository = contactRepository;
+        _mapper = mapper;
     }
 
-    public async Task<List<Contact>?> SortContactsByLastOnline(Guid userId)
+    public async Task<List<ShortContactDto>?> SortContactsByLastOnline(Guid userId, string sortOrder = "desc")
     {
         var contacts = await _contactRepository.AllContactsOfUser(userId);
-        var sortedContacts = contacts.OrderByDescending(c => c.User.LastOnline).ToList();
-        return sortedContacts;
+        IEnumerable<Contact> sortedContactsQuery = sortOrder
+            .Equals("asc", StringComparison.CurrentCultureIgnoreCase)
+            ? contacts
+                .OrderBy(c => c.User.LastOnline)
+            : contacts
+                .OrderByDescending(c => c.User.LastOnline);
+        var sortedContacts = sortedContactsQuery.ToList();
+        return _mapper.Map<List<ShortContactDto>>(sortedContacts);
+    }
+
+
+    public async Task<List<ShortContactDto>?> GetMappedListOfContacts(Guid userId)
+    {
+        var contacts = await _contactRepository.AllContactsOfUser(userId);
+        return _mapper.Map<List<ShortContactDto>>(contacts);
+    }
+
+    public async Task<ShortContactDto> MapResponseOfNewContact(Guid userId, Guid contactId)
+    {
+        var newContact = await _contactRepository.AddToContacts(userId, contactId);
+        return _mapper.Map<ShortContactDto>(newContact);
     }
 }
