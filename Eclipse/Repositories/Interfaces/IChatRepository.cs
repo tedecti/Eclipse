a5ff6e1aee82a7f7147ad2bf60f66aa9f4383c05
tedecti <wolftheguy08@gmail.com>
2024-11-27 11:37:40 +0500

    PinMessage Functionality 📌

--- Eclipse/Repositories/Interfaces/IChatRepository.cs
+++ Eclipse/Repositories/Interfaces/IChatRepository.cs
@@ -4,12 +4,12 @@ namespace Eclipse.Repositories.Interfaces;
 
 public interface IChatRepository
 {
-    Task<Message?> SaveMessageAsync(Message? message);
+    Task<Message?> SaveMessageAsync(Message message);
     Task<ChatRoom?> GetChatRoomAsync(Guid chatRoomId);
     Task<Message?> GetMessageAsync(Guid messageId);
     Task MarkMessageAsReadAsync(Guid messageId);
     Task AddReactionAsync(Guid messageId, string reactionId);
-    Task PinMessageAsync(Guid chatRoomId, Guid messageId);
+    Task<string> PinMessageAsync(Guid chatRoomId, Guid messageId);
     Task<List<Message>> GetChatHistoryAsync(Guid chatRoomId, int skip, int take);
     Task<ChatRoom> CreateOrGetChatRoomAsync(Guid userId1, Guid userId2);
     Task<IEnumerable<ChatRoom>> GetUserChatRoomsAsync(Guid userId);
