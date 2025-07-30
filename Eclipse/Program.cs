59ac95985a0df47ff3665a6b2685facb02b1a038
¨tedecti¨ <wolftheguy08@gmail.com>
2025-07-30 10:43:00 +0500

    ci cd?

--- Eclipse/Program.cs
+++ Eclipse/Program.cs
@@ -104,11 +104,9 @@ public class Program
         }));
         var app = builder.Build();
 
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
 
         app.UseHttpsRedirection();
 
